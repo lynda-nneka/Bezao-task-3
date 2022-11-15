d11def2a0bbe68f682343d110a4bee132cb40589
lynda-nneka <86114880+lynda-nneka@users.noreply.github.com>
2022-11-15 09:58:31 +0100

    Add files via upload

---
+++ Archive 3/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using GuessingGame;
+using LeapYear;
+using PigLatinTranslator;
+
+namespace Bezao
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            PlayGuessingGame guessingGame = new PlayGuessingGame();
+            LeapYearGenerator leapYear = new LeapYearGenerator();
+            TranslateToPigLatin piglatintranslator = new TranslateToPigLatin();
+
+            //To run any of the codes, comment the other one
+
+            //guessingGame.RunGuessingGame();
+            //leapYear.GetLeapYear();
+            piglatintranslator.PlayPigLatinTranslator();
+        }
+
+
+    }
+}
+
+
+
